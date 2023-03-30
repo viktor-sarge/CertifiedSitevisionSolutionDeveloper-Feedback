789a71284e9b8f032a75534e19d3631b60b353ea
Viktor Sarge <viktor.sarge@regionhalland.se>
2023-03-30 12:59:27 +0300

    feat(publish): ✨ Mark previous feedback as old on publish events
    Markerar tidigare feedback som gammal när sidan ompubliceras

--- feedbackRESTApp/src/index.js
+++ feedbackRESTApp/src/index.js
@@ -1,20 +1,25 @@
-import router from '@sitevision/api/common/router';
+import events from "@sitevision/api/common/events";
+import properties from '@sitevision/api/server/Properties';
+import resourceLocatorUtil from '@sitevision/api/server/ResourceLocatorUtil';
+import storage from '@sitevision/api/server/storage';
 
+// Sniffa efter publiceringar och markera sidans feedback som utdaterad
+events.on('sv:publishing:publish', (options) => { 
 
+    // Referenser till aktuell nod och dess ID
+    const updatedNode = resourceLocatorUtil.getNodeByIdentifier(options.node); 
+    const pageID = properties.get(updatedNode); 
 
+    // Hämta tidigare feedback för sidan
+    const feedbackStorage = storage.getCollectionDataStore("feedback");  // Hämta/skapa datakälla i SV
+    const previousFeedback = feedbackStorage.find(`ds.analyzed.page:${pageID}`,100).toArray(); // Plocka ut sidans feedbackposter ur storage
 
-router.get('/myroute/:value', (req, res) => {
-    res.json({ message: `Hello ${req.params.value}` });
-});
-
-router.post('/myroute', (req, res) => {
-    res.json({ message: 'Hello from POST' });
-});
-
-router.put('/myroute', (req, res) => {
-    res.json({ message: 'Hello from PUT' });
-});
-
-router['delete']('/myroute', (req, res) => {
-    res.json({ message: 'Hello from DELETE' });
+    // Gå igenom tidigare feedback och markera som gammal
+    previousFeedback.forEach(entry=>{
+        try {
+            feedbackStorage.set(entry.dsid, {...entry, current:false});
+         } catch (e) {
+            console.log("Ett fel uppstod när tidigare feedback skulle markeras som utdaterad.")
+         }
+    });
 });
